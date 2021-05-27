3e0a472f74b7a54dcff7ac105aaf534545deb0c4
Rokaia Essam <rokaiaessam419@ci.suez.edu.eg>
2021-05-27 04:30:45 +0200

    solve server error/ callback hell

--- Controllers/GithubController.js
+++ Controllers/GithubController.js
@@ -1,20 +1,23 @@
 const fetch = require("node-fetch");
 const moment = require("moment");
 
-const fetchData = () =>{
-   const date = moment().subtract(30,'days').format('YYYY-MM-DD').toString();
-    fetch(`https://api.github.com/search/repositories?q=created:%3E${date}&sort=stars&order=desc&per_page=100`)
-    .then((response) => response.json())
-    .then((data) => {
+async function fetchData() {
+    try {
+        let date = moment().subtract(30, 'days').format('YYYY-MM-DD').toString();
+        const response = await fetch(`https://api.github.com/search/repositories?q=created:%3E${date}&sort=stars&order=desc&per_page=100`);
+        const data = await response.json();
         const filtered_list = withOutNulls(data["items"]);
         const language_list = filterLanguages(filtered_list);
         const repo_list = filterRepos(filtered_list, language_list);
         return repo_list;
-    })
-    .catch((err) => console.log(err));
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
-function withOutNulls(response_list){
+// filter repos with null language
+function withOutNulls(response_list) {
     let new_list = [];
     new_list = response_list.filter(item => {
         return item.language != null;
@@ -22,7 +25,8 @@ function withOutNulls(response_list){
     return new_list;
 }
 
-function filterLanguages(filtered_list){
+// extract distinct languages list
+function filterLanguages(filtered_list) {
     let new_list = [];
     filtered_list.forEach(element => {
         new_list.push(element.language);
@@ -31,16 +35,17 @@ function filterLanguages(filtered_list){
     return new_list;
 }
 
-function filterRepos(filtered_list, language_list){
+// list repo of the same language together 
+function filterRepos(filtered_list, language_list) {
     let new_list = [];
-    language_list.forEach(lang =>{
+    language_list.forEach(lang => {
         let repo_list = filtered_list.filter(item => {
             return item.language == lang;
         });
         let obj = {
-            language : lang,
-            repo_list: repo_list,
-            no_repo: repo_list.length
+            no_repo: repo_list.length,
+            language: lang,
+            repo_list: repo_list
         }
         new_list.push(obj);
     });
--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /** Required External Modules **/
-
 const express = require("express");
 var cors = require('cors');
+const GithubRouter = require("./Routers/GithubRouter");
 
 /** App Variables **/
 
 const app = express();
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || "3000";
 
 /** App Configuration **/
 
 app.use(cors());
+
+/** Routes Definitions **/
+app.use('/api/languageList', GithubRouter);
 
 app.use((req, res, next) => { //logger
     console.log(`Request Url : ${req.url}, Request method : ${req.method}, Date of Request: ${Date()}`);
@@ -22,8 +25,6 @@ app.use( (req, res, next) =>  { //error handler
     res.send({error : "server error"});
 });
 
-
-/** Routes Definitions **/
 
 
 /** Server Activation **/
